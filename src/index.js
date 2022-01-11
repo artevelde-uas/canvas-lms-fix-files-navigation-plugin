d347d37802755a91ae2788113b154e8d9e660bfa
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2022-01-11 22:47:55 +0100

    Add package translations

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { router, dom } from '@artevelde-uas/canvas-lms-app';
 
+import __ from './i18n';
+
 
 export default function ({ expandMyFilesMenu = true } = {}) {
     router.onRoute(['profile.files', 'groups.files', 'courses.files'], async (params, name) => {
@@ -95,5 +97,9 @@ export default function ({ expandMyFilesMenu = true } = {}) {
         }
     });
 
-    return require('../package.json');
+    return {
+        ...require('../package.json'),
+        title: __('package.title'),
+        description: __('package.description')
+    };
 }
